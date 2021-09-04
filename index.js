fa283514005c1e072a5acc08f4006a9117b06786
ritu <ritumkapadia@gmail.com>
2021-09-04 21:43:33 +0530

    A CLI quiz app

---
+++ index.js
@@ -0,0 +1,50 @@
+const rs =require('readline-sync');
+const chalk = require('chalk');
+
+var playerName = rs.question(chalk.blue.bold("Who are you?\n"));
+var score = 0;
+
+console.log(chalk.blue.bold('Hey '+playerName+", how well do you know me? Let's play a game!\n"));
+
+function gamePlay(question , answer){
+  var ans  = rs.question(chalk.cyanBright(question));
+  if(answer.includes(ans.toLowerCase())){
+    console.log(chalk.green('Correct'));    
+    score++;
+  }
+  else{
+    console.log(chalk.red('Wrong'));    
+  }
+    console.log(chalk.blue('\n------------------------------\n'));    
+} 
+
+queArray = [
+  {
+    que:"What's my name? ",
+    ans:["ritu"]
+  },
+  {
+    que:"What's my hobby? ",
+    ans: ["dance", "dancing"]
+  },
+  {
+    que:"What's my favourite color? ",
+    ans:["black", "blue"]
+  },
+  {
+    que:"What was my nickname in college? ",
+    ans:["moon"]
+  },
+  {
+    que:"How many hours of sleep do you need each night to feel good? ",
+    ans:["7", "8"]
+  }
+];
+
+for(let i =0 ; i<queArray.length;i++){
+  gamePlay(queArray[i].que,queArray[i].ans);
+
+}
+console.log('Final score is  : ' + score+"/"+queArray.length);    
+
+
